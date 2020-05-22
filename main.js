b7c711c2ce942d78c58d5719b6b82fb9350a3208
yaakovyitzchak <46662394+yaakovyitzchak@users.noreply.github.com>
2020-05-22 23:38:22 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var http = require("http");
+console.log("OK")
 http.createServer(function(q,r) {
 	r.end("HI >>>?????there MAN what is UP?!" +Date.now());
-}).listen(process.env.PORT || 880);
+}).listen(process.env.PORT || 80);
